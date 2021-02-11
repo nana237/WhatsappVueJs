84eb38eb3386bfe7244b3248935bf39ce32fcb4f
Nana Boris <ndjionkou@gmail.com>
2021-02-11 13:06:19 +0100

    mon premier commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
+import "tailwindcss/tailwind.css"
+//import "tailwindcss";
 
 createApp(App).mount('#app')
